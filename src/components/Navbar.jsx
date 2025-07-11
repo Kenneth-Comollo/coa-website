f7519e123ac4fea4e3090a0ab97f3a5cf7bcf210
Kenneth Comollo <45769147+Kenneth-Comollo@users.noreply.github.com>
2025-07-11 14:04:44 -0400

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const SiteNavbar = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Condo Association</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">Documents</Nav.Link>
+            <Nav.Link href="#">Calendar</Nav.Link>
+            <Nav.Link href="#">Admin</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default SiteNavbar;
